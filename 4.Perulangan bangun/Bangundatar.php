ce4fa5840b082c551a088d0926d2d29da6900508
zamronionyy <zamronitgs@gmail.com>
2023-11-19 20:58:21 +0700

    upload bangun datar dan ss

---
+++ 4.Perulangan bangun/Bangundatar.php
@@ -0,0 +1,30 @@
+<?php
+  echo "<p>Ini PHP Persegi </p>";
+  echo "<pre>";
+ 
+  $persegi = 9;
+  for($i = 1; $i <= $persegi; $i++) {
+    for($j = 1; $j <= $persegi; $j++) {
+      echo " *";
+    }
+    echo "<br>";
+  }
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "<p>Ini PHP Segitiga </p>";
+	$star=9;
+	    for( $a=$star; $a>0;$a--){
+
+	    for($i=1; $i<=$a; $i++){
+
+	}
+	    for($a1=$star;$a1>=$a;$a1--){
+		echo"*";
+	}
+	echo"<br>";
+	}
+
+  echo "</pre>";
+?>
